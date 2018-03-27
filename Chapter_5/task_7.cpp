f675e5c66c773b627a1aad4cee312cfb315f7416
Koniokrad <osowski.grzegorz@gmail.com>
2018-03-27 16:51:01 +0200

    chapter 5 task 7 in progress

---
+++ Chapter_5/task_7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+struct Car {
+  std::string brand;
+  int buildYear;
+};
+
+int main() {
+  int numberOfCars;
+  std::cout<<"Enter number of cars: ";
+  std::cin>>numberOfCars;
+
+  Car *ptr = new Car[numberOfCars];
+
+  for(int i=0; i<numberOfCars; i++) {
+    std::cout<<"Enter vehicle brand: ";
+    getline(std::cin>>std::ws, (*ptr).brand);
+    // std::cin>>(*ptr).brand;
+
+    std::cout<<"Enter a year of build: ";
+    std::cin>>ptr[i].buildYear;
+
+    *(ptr+1);
+  }
+
+  for(int i=0; i<numberOfCars; i++) {
+    std::cout<<ptr[i].buildYear<<" "<<ptr[i].brand<<std::endl;
+  }
+
+  delete [] ptr;
+}

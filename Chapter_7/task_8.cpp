a87e7791d654533e91593d0a6870d5a5e96a7065
gosowski <osowski.grzegorz@gmail.com>
2018-04-12 16:21:52 +0200

    chapter 7 task 8a done

--- Chapter_7/task_8.cpp
+++ Chapter_7/task_8.cpp
@@ -25,13 +25,16 @@ void fillArray(double *begin, const double *end) {
 
 void showArray(const char* s[], double* e) {
   double total = 0.0;
+  double *pTotal = &total;
   double *pt = e;
   int i = 0;
 
   for(pt=e;pt<e+4; pt++) {
     std::cout<<*(s+i)<<" | "<<*pt<<std::endl;
+    *pTotal += *pt;
+
     i++;
   }
 
-  std::cout<<std::endl;
+  std::cout<<"Annual expenses: "<<*pTotal<<std::endl;
 }

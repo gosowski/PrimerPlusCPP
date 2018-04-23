c03fcf84199a2dc1d5f838fb3cd7f4091fb9748a
gosowski <osowski.grzegorz@gmail.com>
2018-04-23 15:51:42 +0200

    chapter 7 task 10 done

---
+++ Chapter_7/task_10.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+double add(double, double);
+double divide(double, double);
+double subtract(double, double);
+
+double calculate(double, double, double (*pf)(double, double));
+
+int main() {
+  double a,b;
+  int choice;
+
+  std::cin>>choice;
+  while(choice !=0) {
+    std::cin>>a>>b;
+
+    switch (choice) {
+      case 1:
+        std::cout<<calculate(a,b,add)<<std::endl;
+        break;
+      case 2:
+        std::cout<<calculate(a,b,divide)<<std::endl;
+        break;
+      case 3:
+        std::cout<<calculate(a,b,subtract)<<std::endl;
+    }
+
+    std::cin>>choice;
+  }
+  
+
+  return 0;
+}
+
+double calculate(double a, double b, double (*pf)(double, double)) {
+  return (*pf)(a,b);
+}
+
+double add(double a, double b) {
+  return a + b;
+}
+
+double divide(double a, double b) {
+  if(b!=0) {
+    return a/b;
+  } else {
+    std::cout<<"Dividing by 0 is prohibitted!"<<std::endl;
+  }
+}
+
+double subtract(double a, double b) {
+  return a - b;
+}
+

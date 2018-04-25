a66089c67827d8fcb5b4d4f7ef9f8f58617bf37e
gosowski <osowski.grzegorz@gmail.com>
2018-04-25 12:22:45 +0200

    Chapter 8 task 2 pointer solution

--- Chapter_8/task_2.cpp
+++ Chapter_8/task_2.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 
-template <typename T> T max5(T []);
+template <typename T> T max5(const T *, const T *);
 
 int main() {
   int arr[] = {1, 2, 9,4,5};
-  double arr2[] = {1.2, 2.5, 3.0, 4.2, 5.1};
+  double arr2[] = {11.2, 2.5, 3.0, 4.2, 5.1};
 
-  std::cout<<max5(arr)<<std::endl;
-  std::cout<<max5(arr2)<<std::endl;
+  std::cout<<max5(arr, arr+5)<<std::endl;
+  std::cout<<max5(arr2, arr2+5)<<std::endl;
 
   return 0;
 }
 
-template <typename T> T max5(T arr[5]) {
+template <typename T> T max5(const T *begin, const T *end) {
   T tmp = 0;
-  for(int i=0; i<5; i++) {
-    if(tmp < arr[i]) {
-      tmp = arr[i];
+  const T *pt;
+  for(pt=begin; pt!=end; pt++) {
+    if(tmp < *pt) {
+      tmp = *pt;
     }
   }
   return tmp;
